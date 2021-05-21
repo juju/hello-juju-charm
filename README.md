fbc7f39e27407888a928bc691b06bf01568c7fad
Jon Seager <jon@sgrs.uk>
2021-05-21 12:29:16 +0100

    Intitial transplant to Charmed Operator Framework - Removes all reactive framework references - Preserves some templated files - Relicense to Apache 2.0 - Copied across config.yaml to preserve charm behaviour

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# `hello-juju-charm`
+# hello-juju-charm
 
-This repository contains the source code for introductory charms
-for new Juju users.
+## Description
+
+TODO: Describe your charm in a few paragraphs of Markdown
+
+## Usage
+
+TODO: Provide high-level usage, such as required config or relations
+
+
+## Developing
+
+Create and activate a virtualenv with the development requirements:
+
+    virtualenv -p python3 venv
+    source venv/bin/activate
+    pip install -r requirements-dev.txt
+
+## Testing
+
+The Python operator framework includes a very nice harness for testing
+operator behaviour without full deployment. Just `run_tests`:
+
+    ./run_tests
---
+++ src/charm.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# Copyright 2021 Canonical
+# See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+
+"""Charm the service.
+
+Refer to the following post for a quick-start guide that will help you
+develop a new k8s charm using the Operator Framework:
+
+    https://discourse.charmhub.io/t/4208
+"""
+
+import logging
+
+from ops.charm import CharmBase
+from ops.framework import StoredState
+from ops.main import main
+from ops.model import ActiveStatus
+
+logger = logging.getLogger(__name__)
+
+
+class HelloJujuCharm(CharmBase):
+    """Charm the service."""
+
+    _stored = StoredState()
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self._stored.set_default(things=[])
+
+    def _on_config_changed(self, _):
+        """Just an example to show how to deal with changed configuration.
+
+        Learn more about config at https://juju.is/docs/sdk/config
+        """
+        current = self.config["thing"]
+        if current not in self._stored.things:
+            logger.debug("found a new thing: %r", current)
+            self._stored.things.append(current)
+
+
+if __name__ == "__main__":
+    main(HelloJujuCharm)
