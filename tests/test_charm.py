cd59ff3f48e90990a4dea81b2a5b7ee2ba3846d7
Jon Seager <jon@sgrs.uk>
2021-05-25 16:11:02 +0100

    Added tests for methods that render settings/systemd units and install apt packages

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -3,18 +3,14 @@
 #
 # Learn more about testing at: https://juju.is/docs/sdk/testing
 
-import os
-import shutil
-import tempfile
+import subprocess
 import unittest
-from unittest.mock import patch
+from unittest import mock
+from unittest.mock import Mock, call, mock_open, patch
 
-import mock
 from charm import APP_PATH, VENV_ROOT, HelloJujuCharm
-from ops.model import MaintenanceStatus
+from ops.model import BlockedStatus, MaintenanceStatus
 from ops.testing import Harness
-
-TMP_DIR = tempfile.mkdtemp()
 
 RENDERED_SETTINGS = """
 
@@ -32,25 +28,24 @@ Description=Hello Juju web application
 After=network.target
 
 [Service]
-WorkingDirectory = /app
+WorkingDirectory = /srv/app
 Restart = always
 RestartSec = 5
-ExecStart=/app/venv/bin/gunicorn \
-            -u www-data \
-            -g www-data \
-            --access-logfile /app/access.log \
-            --error-logfile /app/error.log \
-            --bind 0.0.0.0:80 \
+ExecStart=/srv/app/venv/bin/gunicorn \\
+            -u www-data \\
+            -g www-data \\
+            --access-logfile /srv/app/access.log \\
+            --error-logfile /srv/app/error.log \\
+            --bind 0.0.0.0:80 \\
             hello_juju:app
 ExecReload = /bin/kill -s HUP $MAINPID
 ExecStop = /bin/kill -s TERM $MAINPID
-ExecStartPre = /bin/mkdir /app/run
-PIDFile = /app/run/hello-juju.pid
-ExecStopPost = /bin/rm -rf /app/run
+ExecStartPre = /bin/mkdir /srv/app/run
+PIDFile = /srv/app/run/hello-juju.pid
+ExecStopPost = /bin/rm -rf /srv/app/run
 
 [Install]
-WantedBy = multi-user.target
-"""
+WantedBy = multi-user.target"""
 
 
 class TestCharm(unittest.TestCase):
@@ -74,31 +69,101 @@ class TestCharm(unittest.TestCase):
             self.harness.charm.unit.status, MaintenanceStatus("creating database tables")
         )
 
-    @mock.patch("charm.APP_PATH", TMP_DIR)
-    def test_render_settings_file(self):
-        # Make a new temp directory for the tests
-        os.makedirs(TMP_DIR, exist_ok=True)
-        settings_file = f"{TMP_DIR}/settings.py"
-
+    @mock.patch("os.chmod")
+    @mock.patch("os.chown")
+    @mock.patch("pwd.getpwnam")
+    def test_render_settings_file(self, _pwnam, _chown, _chmod):
         # Set the value that will be written into the settings file
         self.harness.charm._stored.conn_str = "postgresql://test_connection_string"
 
-        with patch("pwd.getpwnam") as mock:
-            # Make sure the uid/gid returned is the current user for the test
-            mock.return_value.pw_uid = os.getuid()
-            mock.return_value.pw_gid = os.getgid()
+        # Setup a mock for the `open` method, set returned data to settings template
+        with open("templates/settings.py.j2", "r") as f:
+            m = mock_open(read_data=f.read())
+
+        # Patch the `open` method with our mock
+        with patch("builtins.open", m, create=True):
+            # Set the uid/gid return values for lookup of 'www-data' user
+            _pwnam.return_value.pw_uid = 35
+            _pwnam.return_value.pw_gid = 35
             # Call the method
             self.harness.charm._render_settings_file()
 
-        # Check the rendered file is as expected
-        with open(settings_file, "r") as f:
-            self.assertEqual(RENDERED_SETTINGS, f.read())
+        # Check the template is opened read-only in the first call to open
+        self.assertEqual(m.call_args_list[0][0], ("templates/settings.py.j2", "r"))
+        # Check the settings file is opened with "w+" mode in the second call to open
+        self.assertEqual(m.call_args_list[1][0], (f"{APP_PATH}/settings.py", "w+"))
+        # Ensure the correct rendered template is written to file
+        m.return_value.write.assert_called_with(RENDERED_SETTINGS)
+        # Ensure that the correct user is lookup up
+        _pwnam.assert_called_with("www-data")
+        # Ensure the file is chmod'd correctly
+        _chmod.assert_called_with(f"{APP_PATH}/settings.py", 0o644)
+        # Ensure the file is chown'd correctly
+        _chown.assert_called_with(f"{APP_PATH}/settings.py", uid=35, gid=35)
 
-        # Ensure the correct file mode/owner was set
-        status = os.stat(settings_file)
-        file_mode = oct(status.st_mode & 0o777)
-        self.assertEqual(file_mode, oct(0o644))
-        self.assertEqual(status.st_uid, os.getuid())
-        self.assertEqual(status.st_gid, os.getgid())
-        # Remove the temporary directory
-        shutil.rmtree(TMP_DIR)
+    @mock.patch("charm.check_call")
+    @mock.patch("os.chmod")
+    def test_render_systemd_unit(self, _chmod, _call):
+        # Create a mock for the `open` method, set the return value of `read` to
+        # the contents of the systemd unit template
+        with open("templates/hello-juju.service.j2", "r") as f:
+            m = mock_open(read_data=f.read())
+
+        # Patch the `open` method with our mock
+        with patch("builtins.open", m, create=True):
+            # Ensure the stored value is clear to test it's set properly
+            self.harness.charm._stored.port = ""
+            # Mock the return value of the `check_call`
+            _call.return_value = 0
+            # Call the method
+            self.harness.charm._render_systemd_unit()
+
+        # Check the state was updated with the port from the config
+        self.assertEqual(self.harness.charm._stored.port, self.harness.charm.config["port"])
+        # Check the template is opened read-only in the first call to open
+        self.assertEqual(m.call_args_list[0][0], ("templates/hello-juju.service.j2", "r"))
+        # Check the systemd unit file is opened with "w+" mode in the second call to open
+        self.assertEqual(m.call_args_list[1][0], ("/etc/systemd/system/hello-juju.service", "w+"))
+        # Ensure the correct rendered template is written to file
+        m.return_value.write.assert_called_with(RENDERED_SYSTEMD_UNIT)
+        # Check the file permissions are set correctly
+        _chmod.assert_called_with("/etc/systemd/system/hello-juju.service", 0o755)
+        # Check that systemd is reloaded to register the changes to the unit
+        _call.assert_called_with(["systemctl", "daemon-reload"])
+
+        # Now check that any existing port in state is respected
+        # Patch the `open` method with our mock
+        with patch("builtins.open", m, create=True):
+            # Ensure the stored value is clear to test it's set properly
+            self.harness.charm._stored.port = 8080
+            # Mock the return value of the `check_call`
+            _call.return_value = 0
+            # Call the method
+            self.harness.charm._render_systemd_unit()
+        # Ensure the rendered template is adjusted to take into consideration the port
+        m.return_value.write.assert_called_with(RENDERED_SYSTEMD_UNIT.replace(":80", ":8080"))
+        self.assertEqual(self.harness.charm._stored.port, 8080)
+
+    @mock.patch("charm.check_output")
+    def test_install_apt_packages(self, _call: Mock):
+        # Set the return code for subprocess.check_output
+        _call.return_code = 0
+        # Call the method with some packages to install
+        self.harness.charm._install_apt_packages(["curl", "vim"])
+        # Ensure the log output is correct
+        self.assertLogs("updating apt cache", level="DEBUG")
+        self.assertLogs("installing apt packages: curl, vim", level="DEBUG")
+        # Check that apt is called with the correct arguments
+        self.assertEqual(
+            _call.call_args_list,
+            [call(["apt-get", "update"]), call(["apt-get", "install", "-y", "curl", "vim"])],
+        )
+        # Now check that if an exception is raised we do the right logging
+        _call.reset_mock()
+        _call.return_value = 1
+        _call.side_effect = subprocess.CalledProcessError(1, "apt")
+        self.harness.charm._install_apt_packages(["curl", "vim"])
+        self.assertLogs("failed to install packages: curl, vim", level="ERROR")
+        self.assertEqual(
+            self.harness.charm.unit.status, BlockedStatus("Failed to install packages")
+        )
